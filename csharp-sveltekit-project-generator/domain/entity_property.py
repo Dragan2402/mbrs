b6d04ace168998e6fedefa6d075fd34bf23f65e0
Dragan Mirkovic <63748840+Dragan2402@users.noreply.github.com>
2024-09-08 16:08:56 +0200

    Feature 8: Added generation of Controllers and DTO's (#9)

--- csharp-sveltekit-project-generator/domain/entity_property.py
+++ csharp-sveltekit-project-generator/domain/entity_property.py
@@ -1,15 +1,21 @@
 class EntityProperty:
     def __init__(
-        self, name: str, property_type: str, is_single_reference: bool = False
+        self,
+        name: str,
+        property_type: str,
+        is_single_reference: bool = False,
+        is_reference: bool = False,
     ) -> None:
         name = name[0].upper() + name[1:]
         self.name = name
         self.type = property_type
         self.is_single_reference = is_single_reference
+        self.is_reference = is_reference
 
     def get_context(self) -> dict:
         return {
             "name": self.name,
             "type": self.type,
             "is_single_reference": self.is_single_reference,
+            "is_reference": self.is_reference,
         }
